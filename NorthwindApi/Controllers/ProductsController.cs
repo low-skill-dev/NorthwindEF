719e397da98e890ca0a67bae18942351e840f463
low-skill-dev <low-skill-dev@github.com>
2024-11-03 20:19:35 +0300

    add models, context and controller

---
+++ NorthwindApi/Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NorthwindApi.DbContexts;
+using NorthwindModels;
+
+namespace NorthwindApi.Controllers;
+
+[ApiController]
+[Route("[controller]/[action]")]
+public class ProductsController : ControllerBase
+{
+	// pre-compiled query
+	// stored procedure
+	// split query
+
+	private readonly NorthwindContext _context;
+	public ProductsController(NorthwindContext context)
+	{
+		_context = context;
+	}
+
+	[HttpGet]
+	public async Task<List<Product>> GetProducts()
+	{
+		return await _context.Products.ToListAsync();
+	}
+}
